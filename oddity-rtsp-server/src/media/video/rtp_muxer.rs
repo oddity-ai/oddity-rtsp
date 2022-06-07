f976a0577fbd109c1b6f2fd3cd58af0c91511fc0
Gerwin van der Lugt <account+git@gerwin3.com>
2022-06-07 15:41:42 +0200

    reverted some stuff that is not necessary for TCP/interleaved only

--- oddity-rtsp-server/src/media/video/rtp_muxer.rs
+++ oddity-rtsp-server/src/media/video/rtp_muxer.rs
@@ -1,21 +1,14 @@
 //! Async wrapper functions for [`oddity_video::RtpMuxer`].
 
-use std::net;
-
 use tokio::task;
 
 use oddity_video::{self as video, RtpMuxer};
 
 type Result<T> = std::result::Result<T, video::Error>;
 
-pub async fn make_rtp_muxer(
-  dest: net::Ip
-) -> Result<RtpMuxer> {
-  // TODO transport client_addr and port
+pub async fn make_rtp_muxer() -> Result<RtpMuxer> {
   task::spawn_blocking(move || {
-      RtpMuxer::new(
-
-      )
+      RtpMuxer::new()
     })
     .await
     .unwrap()
